1089c91dfbe1b4525f8f41f5c96b52bb3e71e75b
Otto Hanski <otto.hanski@gmail.com>
2024-02-16 15:23:56 +0200

    Some thoughts on hit calc in SampleCell

--- SampleCell.py
+++ SampleCell.py
@@ -1,6 +1,7 @@
 """
 File for defining the sample cell geometry. The cell has a 
-z-coordinate and a radius as a function of z. The profile should
+z-coordinate and a radius as a function of z. We always assume
+cylindrical symmetry. The profile should
 be defined as a np-array of length n of r values corresponding to z values. 
 Constructor of the form SampleCell(samples, z, r), with z and r numpy arrays 
 of length samples.
@@ -11,11 +12,40 @@ angles in a numpy array of length n-1 for quick lookup during photon reflection 
 
 For the same buckets, define a wavelength-dependent absorption probability on the wall
 as well as probabilities of specular and diffuse reflection.
+
+To account for non-cylindrical (but still cylindrically symmetric) cells, we must check 
+the wall hits in 3D, as hits to an angled wall will change the direction of the photon 
+for both angles.
 """
 
+import numpy as np
+
 class SampleCell:
-    def __init__(self, samples, z, r):
+    def __init__(self, z, r, z_samples, r_samples):
+        self.z = z
+        self.r = r
+        self.zlen = z_samples
+        self.wall = np.zeros(z_samples-1)
+
+        for i in range(z_samples-1):
+            self.wall[i] = np.arctan((r[i+1]-r[i])/(z[i+1]-z[i]))
+    
+    def hit_wall(self, z_start, r_start, theta, phi):
+        # Check location of wall hit, return new z, r, theta, phi
         pass
+
+        
+    def get_z_index(self, z):
+        # Return the index of z in the z array
+        pass
+        """
+        def find_nearest(array,value):
+            idx = np.searchsorted(array, value, side="left")
+            if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+                return array[idx-1]
+            else:
+                return array[idx]
+        """
 
     def get_wall_angle(self, z):
         # Return the angle of the wall at z
