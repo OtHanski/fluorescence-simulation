Fixed exit point calculation

SampleCell.py
@@ -126,10 +126,12 @@ class SampleCell:
         if len(idhits) == 0:
             if z_dir:
                 if debug: print(f"Exit at {pos_path[-1,0], pos_path[-1,1], self.z[-1]}")
-                return np.array([pos_path[-1,0], pos_path[-1,1], self.z[-1]]), direction, True, "exit"
+                exitpos = position + transit * (self.z[-1] - z_start)
+                return exitpos, direction, True, "exit"
             else:
                 if debug: print(f"Exit at {pos_path[0,0], pos_path[0,1], self.z[0]}")
-                return np.array([pos_path[0,0], pos_path[0,1], self.z[0]]), direction, True, "exit"
+                exitpos = position + transit * (self.z[0] - z_start)
+                return exitpos, direction, True, "exit"
         # idhits returns all "hits", we only want first one.
         idhit = idhits[0][0]
 
@@ -195,7 +197,7 @@ class SampleCell:
         Returns:
             np.ndarray (x,y,z): Random unit vector"""
         theta = np.random.rand() * 2 * np.pi
-        phi = np.random.rand() * np.pi
+        phi = (np.random.rand()-0.5) * np.pi
         z = np.sin(phi)
         x = np.cos(theta) * np.cos(phi)
         y = np.sin(theta) * np.cos(phi)

photon.py
@@ -34,6 +34,7 @@ class photon:
                  id = 0):
         
         self.debug = 0
+        self.logold = 1
 
         # Initialize photon parameters
         self.sampCell = sampCell
@@ -71,11 +72,19 @@ class photon:
         Returns (position, direction, num of wall interactions, wavelength, exit/absorp)."""
         try:
             while True:
+                if self.logold:
+                    self.oldevent = self.event
+                    self.oldpos = self.pos
+                    self.olddirec = self.direc
+
                 hit = self.sampCell.hit_wall(position=self.pos, direction=self.direc, 
                                             wavelength=self.wavelength, debug = self.debug)
                 self.event = hit[3]
                 if hit[3] == "exit":
-                    if self.debug: print(f"Photon exited at {hit[0]} with direction {hit[1]}")
+                    if self.debug: 
+                        print(f"Photon exited at {hit[0]} with direction {hit[1]}, old event {self.oldevent}, old pos {self.oldpos}, old direc {self.olddirec}")
+                    if ((hit[0] == np.array([0,0,0])).all() or (hit[0] == np.array([0,0,10])).all()): 
+                        print(f"Photon exited at {hit[0]} with direction {hit[1]}, \nold event {self.oldevent}, old pos {self.oldpos}, old direc {self.olddirec}")
                     self.pos = hit[0]
                     self.direc = hit[1]
                     return hit[0], hit[1], self.bounces, self.wavelength, hit[3]

simulation.py
@@ -20,7 +20,7 @@ filename = "simulation.dat"
 # Number of particles to simulate
 simulations = 1000
 # Number of wall sections to divide the cell into (1 to n)
-wall_sections = 150
+wall_sections = 1500
 # Cell parameters:
 shape = "cylinder"
 r_cell = 5E-3 # Radius of the cell [m]
@@ -78,7 +78,7 @@ def main(simulations = simulations, wall_sections = wall_sections, r_cell = r_ce
         if (i+1) % int(simulations/10) == 0:
             print(f"\nSimulating photon {i+1}/{simulations}")
         pos = randomGasPoint()
-        phot = photon(sampCell=cell, position=pos, direction = np.array([0.15,0,1]), id = i+1)
+        phot = photon(sampCell=cell, position=pos, id = i+1)
         #try:
         result = phot.simulate()
         # Append result to file